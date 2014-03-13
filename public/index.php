a1abbfee71fb0450e7df59d06d6467ff3706a9da
Frank Hildebrandt <frank.hildebrandt@me.com>
2014-03-13 11:21:23 +0100

    Silex bootstrapp..

--- public/index.php
+++ public/index.php
@@ -5,4 +5,7 @@ define ("NEDI_API_APPLICATION_ROOT", realpath(__DIR__."/.."));
 require_once NEDI_API_APPLICATION_ROOT."/vendor/autoload.php";
 
 $app = new \Silex\Application();
+$app->get("/", function() {
+        return "It works!";
+    });
 $app->run();
